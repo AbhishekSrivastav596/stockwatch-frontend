0597c7074cc5be0fd2e8eccfbe22944e86a4d183
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2026-02-02 22:45:13 +0530

    Debug: Session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,20 @@
-"use client"
-
-import Navbar from "@/components/ui/navbar"
-import WatchlistTable from "@/components/ui/watchlist-table"
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Navbar from "@/components/ui/navbar";
+import WatchlistTable from "@/components/ui/watchlist-table";
 
 export default function Dashboard() {
+  const session = useSession();
+  const router = useRouter();
+  console.log("Session data:", session);
+  if (session.status === "unauthenticated") {
+    router.push("/");
+  }
   return (
     <div className="p-6 space-y-6">
       <Navbar />
-      <WatchlistTable />
+      {session.status === "authenticated" && <WatchlistTable />}
     </div>
-  )
+  );
 }
